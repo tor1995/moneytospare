04b350e438f66d044e6b940a46bbf1cce47d696f
Tav Bear <tavbear@gmail.com>
2023-02-11 11:34:27 +0200

    changed Form

--- components/FeedbackForm.js
+++ components/FeedbackForm.js
@@ -17,14 +17,9 @@ export default function FeedbackForm() {
             </label>
         </p>
   
-        <label htmlFor="name">Name</label>
-        <input id="name" className={styles['form-field']} type="text" name="name" />
+        <label htmlFor="ףסכ">Name</label>
+        <input id="name" className={styles['form-field']} type="text" name="name" required />
 
-        <label htmlFor="email">Email</label>
-        <input id="email" className={styles['form-field']} type="email" name="email" required />
-
-        <label htmlFor="feedback">What is your feedback?</label>
-        <textarea id="feedback" className={styles['form-field']} wrap="soft" name="feedback" required></textarea>
         <button className={styles.button} type="submit">Submit</button>
       </form>
   )
